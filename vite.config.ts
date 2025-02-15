d238d560114d0b8ada80a2b2a5d9a7634c88c8fa
郑明凯 <40195003+zheng-mingkai@users.noreply.github.com>
2025-02-15 22:23:02 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,31 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
-import { viteMockServe } from 'vite-plugin-mock'; // 新增
+import { viteMockServe } from 'vite-plugin-mock'; 
 
-/**
- * 详情见 vitejs 文档：https://vitejs.dev/config/
- */
-export default defineConfig({
-  plugins: [vue()],
-  base: "/", // 配置相对地址或绝对地址，此处应为绝对地址，若将 Web 部署到 Nginx 所在的目录为 nginx-1.17.8/html/xxx ，则这个 base 的值就为 /xxx/
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
-      assets: resolve(__dirname, "./src/assets"),
-    },
-  },
-  server: {
-    host: "0.0.0.0", // 地址
-    port: 5173, // 端口
-    proxy: {
-      // 项目 v1 的服务端接口地址
-      "/v1/api": {
-        target: "http://127.0.0.1:8091/",
-        changeOrigin: true,
-        secure: false,
-        ws: true,
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [vue(), viteMockServe({
+      localEnabled: mode === 'development', // 仅在开发环境启用 mock
+    })],
+    base: mode === '/zheng-mingkai.github.io/',
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+        assets: resolve(__dirname, "./src/assets"),
       },
     },
-  },
+    server: {
+      host: "0.0.0.0",
+      port: 5173,
+      proxy: {
+        "/v1/api": {
+          target: "http://127.0.0.1:8091/",
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        },
+      },
+    },
+  };
 });
